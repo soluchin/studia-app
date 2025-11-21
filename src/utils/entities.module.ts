3de389ef64716ddbacd96dc2cb1f766f52bee654
soluchin <rafisolichin@gmail.com>
2025-11-21 15:26:50 +0700

    manage project set up

--- src/utils/entities.module.ts
+++ src/utils/entities.module.ts
@@ -3,12 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { postgresConfig } from '../config/postgres.config';
 import { Parent } from '../entities/parent.entity';
 import { Student } from '../entities/student.entity';
-import { sqliteConfig } from 'src/config/sqllite.config';
 
 @Module({
     imports: [
-        // TypeOrmModule.forRoot(postgresConfig),
-        TypeOrmModule.forRoot(sqliteConfig),
+        TypeOrmModule.forRoot(postgresConfig),
         TypeOrmModule.forFeature([
             Parent,
             Student
